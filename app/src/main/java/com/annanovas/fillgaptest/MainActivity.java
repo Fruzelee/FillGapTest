5b267914753a7ffc9ba8288123e87c399d3f3087
fruzelee <fazlerabbicse@gmail.com>
2016-12-07 17:48:18 +0600

    v1.2

--- app/src/main/java/com/annanovas/fillgaptest/MainActivity.java
+++ app/src/main/java/com/annanovas/fillgaptest/MainActivity.java
@@ -3,8 +3,13 @@ package com.annanovas.fillgaptest;
 import android.app.Activity;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -12,11 +17,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    TextView textView1, textView2, textView3;
-
-    String speech = "This is a string";
-
+    String speech = "Fazle Rabbi";
     String[] result;
+    private LinearLayout layoutRoot, childLinearLayout;
+    private int layoutWidth, layoutHeight;
     private ArrayList<WordObject> wordArrayList = new ArrayList<>();
 
     @Override
@@ -24,65 +28,64 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        textView1 = (TextView) findViewById(R.id.textView1);
-//        textView2 = (TextView) findViewById(R.id.textView2);
-        textView3 = (TextView) findViewById(R.id.textView3);
+        layoutRoot = (LinearLayout) findViewById(R.id.layout_root);
+        final ViewTreeObserver observer = layoutRoot.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                wordArrayList = getAllWords();
+                layoutWidth = layoutRoot.getWidth();
+                layoutHeight = layoutRoot.getHeight();
 
-        wordArrayList = getAllWords();
-
-//        //Converting string to char array.
-//        data = value.toCharArray();
-//
-//        //Showing string on screen using textView.
-//        textView1.setText("String = " + value);
-//
-//        //Showing char array on screen with Comma.
-//
-//        textView2.setText("Char Array = ");
-//
-//        for (int i = 0; i < data.length; i++) {
-//
-//            textView2.setText(textView2.getText() + "" + data[i] + ", ");
-//        }
-//
-//        //Showing char array on screen without Comma.
-//
-//        textView3.setText("Char Array without Comma = ");
-//
-//        for (int i = 0; i < data.length; i++) {
-//
-//            textView3.setText(textView3.getText() + "" + data[i]);
+                //DisplayMetrics displaymetrics = new DisplayMetrics();
 
 
-//        for (int x = 0; x < result.length; x++) {
-//            System.out.println(result[x]);
-//
-//            textView3.setText(textView3.getText() + " " + result[x]);
-//
-//            Log.e("Result", result[x]);
-//
-//            Paint paint = new Paint();
-//            Rect bounds = new Rect();
-//
-//            int text_height = 0;
-//            int text_width = 0;
-//
-//            paint.setTypeface(Typeface.DEFAULT);// your preference here
-//            paint.setTextSize(25);// have this the same as your text size
-//
-//            //String text = "Some random text";
-//
-//            paint.getTextBounds(result[x], 0, result[x].length(), bounds);
-//
-//            text_height = bounds.height();
-//            text_width = bounds.width();
-//
-//            Log.e("Index", String.valueOf(text_height));
-//            Log.e("Index", String.valueOf(text_width));
-//
-//        }
+                float ht_dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, layoutWidth, getResources().getDisplayMetrics());
+                float wt_dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, layoutHeight, getResources().getDisplayMetrics());
 
 
+
+                Log.d("Log", "Width: " + layoutWidth);
+                Log.d("Log", "Height: " + layoutHeight);
+
+                Log.d("Log", "Converted Width: " + ht_dp);
+                Log.d("Log", "Converted Height: " + wt_dp);
+
+
+                int x = 0, y = 0;
+                for (int i = 0; i < wordArrayList.size(); i++) {
+                    if (x == 0) {
+                        childLinearLayout = new LinearLayout(MainActivity.this);
+                        childLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+                    }
+
+                    x = x + wordArrayList.get(i).getWidth();
+                    Log.d("Log", "X: " + x);
+
+                    if (x < ht_dp) {
+                        TextView DisplayStringArray = new TextView(MainActivity.this);
+                        DisplayStringArray.setTextSize(25);
+                        DisplayStringArray.setGravity(Gravity.CENTER);
+                        DisplayStringArray.setTextColor(getResources().getColor(R.color.colorAccent));
+                        DisplayStringArray.setText(wordArrayList.get(i).getText());
+                        childLinearLayout.addView(DisplayStringArray);
+                        DisplayStringArray.append(" ");
+                        Log.e("If is here: ", wordArrayList.get(i).getText());
+                    } else {
+                        layoutRoot.addView(childLinearLayout);
+                        x = 0;
+                        y = y + wordArrayList.get(i).getHeight();
+                    }
+
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    layoutRoot.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    layoutRoot.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+            }
+        });
     }
 
     private ArrayList<WordObject> getAllWords() {
@@ -100,50 +103,25 @@ public class MainActivity extends Activity {
 
             //String text = "Some random text";
             paint.getTextBounds(result[x], 0, result[x].length(), bounds);
-            word_height = bounds.height();
             word_width = bounds.width();
-            Log.e("Index", String.valueOf(word_height));
-            Log.e("Index", String.valueOf(word_width));
+            word_height = bounds.height();
+
+
+            float converted_word_width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, word_width, getResources().getDisplayMetrics());
+            float converted_word_height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, word_height, getResources().getDisplayMetrics());
+
+
+            Log.e("width in dp: ", String.valueOf(converted_word_width));
+            Log.e("height in dp: ", String.valueOf(converted_word_height));
             WordObject wordObject = new WordObject();
             wordObject.setText(result[x]);
-            wordObject.setHeight(word_height);
-            wordObject.setWidth(word_width);
+            wordObject.setWidth((int) converted_word_width);
+            wordObject.setHeight((int) converted_word_height);
+
             words.add(wordObject);
+
         }
         return words;
     }
 
-
-//        Paint p = new Paint();
-//        Rect bounds = new Rect();
-//
-//        for (float f = 10; f < 40; f += 1f) {
-//            p.setTextSize(f);
-//            p.getTextBounds(value, 0, value.length(), bounds);
-//            Log.d("Test", String.format(Size %f, measureText %f, getTextBounds %d), f, p.measureText(value), bounds.width());
-//        }
-
-//
-//        String Str = new String(value);
-//        System.out.print("Found Index :");
-//        System.out.println(Str.indexOf('_'));
-//
-//        Log.e("Index", String.valueOf(Str.indexOf('_')));
-//
-//
-//        Rect bounds = new Rect();
-//        Paint textPaint = textView3.getPaint();
-//        textPaint.getTextBounds(value, 0, Str.indexOf('_'), bounds);
-//        int height = bounds.height();
-//        int firstWidth = bounds.width();
-//        textPaint.getTextBounds(value, Str.indexOf('_') + 3, value.length(), bounds);
-//        int secondWidth = bounds.width();
-//
-//
-//        Log.e("String Bounds", "firstWidth " + firstWidth + " secondWidth " + secondWidth);
-
-
 }
-
-
-
