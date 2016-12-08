553e2244e55a42eed043b4046eceae5decbca67b
fruzelee <fazlerabbicse@gmail.com>
2016-12-08 19:04:11 +0600

    v1.3

--- app/src/main/java/com/annanovas/fillgaptest/MainActivity.java
+++ app/src/main/java/com/annanovas/fillgaptest/MainActivity.java
@@ -1,27 +1,36 @@
 package com.annanovas.fillgaptest;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
-    String speech = "Fazle Rabbi";
+    String speech = "First thing I should have ### is that charAt is a method and assigning value to it using equal sign won't do ### . If a string is immutable, charAt method, to make ### to the string object must receive an argument containing the new character. Unfortunately, string is immutable. To modify the string, I ### to use StringBuilder as suggested by Mr. Petar Ivanov.";
+
     String[] result;
-    private LinearLayout layoutRoot, childLinearLayout;
+    private TextView tvWord;
+    private LinearLayout layoutRoot;
+    private RelativeLayout childRelativeLayout;
+    private Button buttonFillGap;
+
     private int layoutWidth, layoutHeight;
+
     private ArrayList<WordObject> wordArrayList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,49 +45,66 @@ public class MainActivity extends Activity {
                 wordArrayList = getAllWords();
                 layoutWidth = layoutRoot.getWidth();
                 layoutHeight = layoutRoot.getHeight();
-
-                //DisplayMetrics displaymetrics = new DisplayMetrics();
-
-
-                float ht_dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, layoutWidth, getResources().getDisplayMetrics());
-                float wt_dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, layoutHeight, getResources().getDisplayMetrics());
+                Log.e("Log", "Width: " + layoutWidth);
+                Log.e("Log", "Height: " + layoutHeight);
 
 
-
-                Log.d("Log", "Width: " + layoutWidth);
-                Log.d("Log", "Height: " + layoutHeight);
-
-                Log.d("Log", "Converted Width: " + ht_dp);
-                Log.d("Log", "Converted Height: " + wt_dp);
-
-
+                childRelativeLayout = new RelativeLayout(MainActivity.this);
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+                //childRelativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                childRelativeLayout.setBackgroundColor(getRandomColor());
                 int x = 0, y = 0;
                 for (int i = 0; i < wordArrayList.size(); i++) {
-                    if (x == 0) {
-                        childLinearLayout = new LinearLayout(MainActivity.this);
-                        childLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
-                    }
+                    if (!wordArrayList.get(i).getText().equals("###")) {
+                        tvWord = new TextView(MainActivity.this);
+                        tvWord.setTextSize(25);
+                        //tvWord.setTextColor(getResources().getColor(R.color.colorAccent));
+                        tvWord.setTextColor(getRandomColor());
+                        tvWord.setText(wordArrayList.get(i).getText());
+                        tvWord.append(" ");
+                        tvWord.measure(0, 0);
+                        tvWord.getMeasuredWidth();
+                        tvWord.getMeasuredHeight();
+                        Log.e("Text View width: ", String.valueOf(tvWord.getMeasuredWidth()));
+                        Log.e("Text View height: ", String.valueOf(tvWord.getMeasuredHeight()));
+                        Log.e("Value of X", "X " + x);
+                        Log.e("Value of Y", "Y " + y);
+                        if ((x + tvWord.getMeasuredWidth()) > layoutWidth - 2 * getResources().getDimension(R.dimen.activity_horizontal_margin)) {
+                            y = y + tvWord.getMeasuredHeight();
+                            x = 0;
+                        }
+                        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);//(RelativeLayout.LayoutParams) tvWord.getLayoutParams();
+                        lp.setMargins(x, y, 0, 0);
 
-                    x = x + wordArrayList.get(i).getWidth();
-                    Log.d("Log", "X: " + x);
+                        tvWord.setLayoutParams(lp);
+                        childRelativeLayout.addView(tvWord);
 
-                    if (x < ht_dp) {
-                        TextView DisplayStringArray = new TextView(MainActivity.this);
-                        DisplayStringArray.setTextSize(25);
-                        DisplayStringArray.setGravity(Gravity.CENTER);
-                        DisplayStringArray.setTextColor(getResources().getColor(R.color.colorAccent));
-                        DisplayStringArray.setText(wordArrayList.get(i).getText());
-                        childLinearLayout.addView(DisplayStringArray);
-                        DisplayStringArray.append(" ");
-                        Log.e("If is here: ", wordArrayList.get(i).getText());
+                        x = x + tvWord.getMeasuredWidth();
                     } else {
-                        layoutRoot.addView(childLinearLayout);
-                        x = 0;
-                        y = y + wordArrayList.get(i).getHeight();
-                    }
+                        buttonFillGap = new Button(MainActivity.this);
+                        buttonFillGap.setText("Enter");
+                        buttonFillGap.measure(0, 0);
+                        buttonFillGap.getMeasuredWidth();
+                        buttonFillGap.getMeasuredHeight();
+                        Log.e("Button width: ", String.valueOf(buttonFillGap.getMeasuredWidth()));
+                        Log.e("Button height: ", String.valueOf(buttonFillGap.getMeasuredHeight()));
+                        Log.e("Value of X", "X " + x);
+                        Log.e("Value of Y", "Y " + y);
+                        if ((x + buttonFillGap.getMeasuredWidth()) > layoutWidth - 2 * getResources().getDimension(R.dimen.activity_horizontal_margin)) {
+                            y = y + buttonFillGap.getMeasuredHeight();
+                            x = 0;
+                        }
+                        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);//(RelativeLayout.LayoutParams) tvWord.getLayoutParams();
+                        layoutParams.setMargins(x, y, 0, 0);
+//
+                        buttonFillGap.setLayoutParams(layoutParams);
+                        childRelativeLayout.addView(buttonFillGap);
 
+                        x = x + buttonFillGap.getMeasuredWidth();
+                    }
                 }
 
+                layoutRoot.addView(childRelativeLayout, params);
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
                     layoutRoot.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 } else {
@@ -86,6 +112,17 @@ public class MainActivity extends Activity {
                 }
             }
         });
+    }
+
+    public int getRandomColor() {
+        Random rnd = new Random();
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+    public int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
+        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return px;
     }
 
     private ArrayList<WordObject> getAllWords() {
@@ -98,28 +135,16 @@ public class MainActivity extends Activity {
             Rect bounds = new Rect();
             int word_height = 0;
             int word_width = 0;
-//            paint.setTypeface(Typeface.DEFAULT);// your preference here
-//            paint.setTextSize(25);// have this the same as your text size
-
-            //String text = "Some random text";
             paint.getTextBounds(result[x], 0, result[x].length(), bounds);
-            word_width = bounds.width();
-            word_height = bounds.height();
-
-
-            float converted_word_width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, word_width, getResources().getDisplayMetrics());
-            float converted_word_height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, word_height, getResources().getDisplayMetrics());
-
-
-            Log.e("width in dp: ", String.valueOf(converted_word_width));
-            Log.e("height in dp: ", String.valueOf(converted_word_height));
+            word_width = dpToPx(bounds.width());
+            word_height = dpToPx(bounds.height());
+            Log.d("String width in dp: ", String.valueOf(word_width));
+            Log.d("String height in dp: ", String.valueOf(word_height));
             WordObject wordObject = new WordObject();
             wordObject.setText(result[x]);
-            wordObject.setWidth((int) converted_word_width);
-            wordObject.setHeight((int) converted_word_height);
-
+            wordObject.setWidth(word_width);
+            wordObject.setHeight(word_height);
             words.add(wordObject);
-
         }
         return words;
     }
